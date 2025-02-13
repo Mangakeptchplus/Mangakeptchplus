f4407fe41568ddb30277124ac7a69a1b48065b9d
Mangakeptchplus <baszza33654@gmail.com>
2025-02-13 20:17:51 +0700

    Add files via upload

---
+++ html.js
@@ -0,0 +1,8 @@
+<button onclick="goToReader()">อ่านตอนแรก</button>
+
+<script>
+    function goToReader() {
+        // นำทางไปยังหน้าใหม่
+        window.location.href = "reader_page.html"; // เปลี่ยน "reader_page.html" เป็น URL หรือ path ของหน้าที่ต้องการ
+    }
+</script>
